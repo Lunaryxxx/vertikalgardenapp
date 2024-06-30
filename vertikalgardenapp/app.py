1e3625384a04266335c0e02f25ff73c989e24f6b
Lunaryxxx <121535091+Lunaryxxx@users.noreply.github.com>
2024-06-30 23:07:27 +0700

    Update app.py

--- vertikalgardenapp/app.py
+++ vertikalgardenapp/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, Response
-import tensorflow as tf
 import numpy as np
+import joblib
 import os
 
 app = Flask(__name__)
 
-# Load your trained model
-model = tf.keras.models.load_model('harvest_prediction_model.keras')
+# Load the trained Random Forest model
+model = joblib.load('ModelRFR.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,11 +17,11 @@ def predict():
     # Convert list to NumPy array and reshape to 2D
     sensor_readings = np.array(sensor_readings).reshape(1, -1)
 
-    # Make prediction using your model
+    # Make prediction using the Random Forest model
     prediction = model.predict(sensor_readings)
 
     # Round the prediction to the nearest integer
-    prediction_rounded = round(prediction[0][0])
+    prediction_rounded = round(prediction[0])
 
     # Send back the result as plain text with the desired format
     return Response(f"{prediction_rounded} Days", mimetype='text/plain')
